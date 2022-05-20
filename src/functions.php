f6be2a6f280628d108e1817d04e1b419a3dc06b0
dpetrohilos <dylanpetrohilos@gmail.com>
2022-05-19 20:31:17 -0400

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -466,15 +466,6 @@ function html5_shortcode_demo_2( $atts, $content = null ) {
 
 // woo commerce 
 
-
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
-
-
-
-add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
-add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
-
 function my_theme_wrapper_start() {
     echo '<section id="main">';
 }
