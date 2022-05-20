f94ec45c69d7682126089b5bea1476fa9019d5ef
dpetrohilos <dylanpetrohilos@gmail.com>
2022-05-19 20:32:15 -0400

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -283,7 +283,7 @@ function html5_blank_view_article( $more ) {
 }
 
 // Remove Admin bar
-// function remove_admin_bar() {
+function remove_admin_bar() {
     return false;
 }
 
@@ -393,7 +393,7 @@ add_filter( 'the_category', 'remove_category_rel_from_category_list' ); // Remov
 add_filter( 'the_excerpt', 'shortcode_unautop' ); // Remove auto <p> tags in Excerpt (Manual Excerpts only)
 add_filter( 'the_excerpt', 'do_shortcode' ); // Allows Shortcodes to be executed in Excerpt (Manual Excerpts only)
 add_filter( 'excerpt_more', 'html5_blank_view_article' ); // Add 'View Article' button instead of [...] for Excerpts
-// add_filter( 'show_admin_bar', 'remove_admin_bar' ); // Remove Admin bar
+add_filter( 'show_admin_bar', 'remove_admin_bar' ); // Remove Admin bar
 add_filter( 'style_loader_tag', 'html5_style_remove' ); // Remove 'text/css' from enqueued stylesheet
 add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10 ); // Remove width and height dynamic attributes to thumbnails
 add_filter( 'post_thumbnail_html', 'remove_width_attribute', 10 ); // Remove width and height dynamic attributes to post images
