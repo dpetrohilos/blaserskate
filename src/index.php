299e0749103297c57a94b2e283b41f0c8663aa50
dpetrohilos <dylanpetrohilos@gmail.com>
2022-07-28 15:59:18 -0400

    type scale

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
 <section class="grid-item-1 col-1">
 <header>
 <hr/> <div class="over-text">2022</div>
-<h1 class="title">New Collection</h1>
+<h1 class="section-title">New Collection</h1>
 <p>Louisville, KY is known for two things. Very strong baseball bats, and lovingly handcrafted skateboards. Come shred with us!</p>
 <a class="button primary" href="/store">Explore</a>
 
@@ -27,7 +27,7 @@
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/img_1.jpg"/>
 </div>
 <div class="grid-item-2 col-2">
-<blockquote>
+<blockquote class="pq">
 We do this for the love of skateboarding. Blaser Boards was founded in 2022 by a group of friends. Each deck is hand drawn, hand painted and lovingly handcrafted.
 </blockquote>
 <cite>–Justin Cornett, 
@@ -36,7 +36,7 @@ Louisville, KY </cite>
 </div>
 </div>
 <div class="grid grid-row-3">
-<header class="grid-item-1 title">
+<header class="grid-item-1">
 <h2 class="section-title">Shred The Gnar </h2>
 </header>
 <div class="grid-item-2 col-1"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/img_2.jpg"/>
@@ -53,8 +53,8 @@ Founder, Louisville, KY </cite>
 </div>
 </div>
 <div class="grid grid-row-3">
-<div class="grid-item-1 col-1">
-<h3 class="section-title">Our Store </h3>
+<div class="grid-item-1 col-1 section-title">
+<h3 class="title">Our Store </h3>
 <p>Put simply, Blaser Boards are made from the strongest wood you can get. Sustainably sourced, slow grown, American maple from Northern Wisconsin gives each board has its own unique signature. Perfect for every free spirited adventurer who can’t be tamed.</p>
 <a href="/decks" class="buttons secondary">Decks</a>
 <a href="/clothing" class="buttons secondary">Clothing</a>
@@ -73,18 +73,18 @@ Founder, Louisville, KY </cite>
 	</main>
 <div class="grid-row-5 grid">
 <header class="grid-item-1 title">
-<h5>The Squad
+<h5 class="section-title">The Squad
 </h5>
 </header>
 <div class="grid grid-item-2" >
 <div class="grid-item-1 kent col-1">
-<p>Korey Blaser</p>
-<p>Sar Rats </p>
-<p>KRIN Fernandez</p>
+<p class="sub-title">Korey Blaser</p>
+<p class="sub-title">Sar Rats </p>
+<p class="sub-title">KRIN Fernandez</p>
 </div>
 <div class="grid-item-2 kent col-2">
-<p>Justin Cornett</p>
-<p>Holden Turner</p>
+<p class="sub-title">Justin Cornett</p>
+<p class="sub-title">Holden Turner</p>
 </div>
 </div>
 </div>
