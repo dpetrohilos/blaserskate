7bd69ac8ffee285befeec8c5e49fb57e3c5035f2
dpetrohilos <dylanpetrohilos@gmail.com>
2022-08-06 16:25:43 -0400

    test

--- src/header.php
+++ src/header.php
@@ -69,4 +69,25 @@
     </ul>
   </nav>
   </div>
-
+  <section class="headline">
+    <h1>Responsive Navigation</h1>
+    <p>Using CSS grid and flexbox to easily build navbars!</p>
+  </section>
+  <section class="features">
+    <div class="feature-container">
+      <img src="https://cdn-images-1.medium.com/max/2000/1*HFAEJvVOq4AwFuBivNu_OQ.png" alt="Flexbox Feature">
+      <h2>Flexbox Featured</h2>
+      <p>This pen contains use of flexbox for the headline and feature section! We use it in our mobile navbar and show the power of mixing css grid and flexbox.</p>
+    </div>
+    <div class="feature-container">
+      <img src="https://blog.webix.com/wp-content/uploads/2017/06/20170621-CSS-Grid-Layout-710x355-tiny.png" alt="Flexbox Feature">
+      <h2>CSS Grid Navigation</h2>
+      <p>While flexbox is used for the the mobile navbar, CSS grid is used for the desktop navbar showing many ways we can use both.</p>
+    </div>
+    <div class="feature-container">
+      <img src="https://blog.edx.org/wp-content/uploads/2015/09/null-1.jpg" alt="Flexbox Feature">
+      <h2>Basic HTML5</h2>
+      <p>This pen contains basic html to setup the page to display the responsive navbar.</p>
+    </div>
+  </section>
+</div>
