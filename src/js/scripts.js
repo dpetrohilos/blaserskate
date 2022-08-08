4d2ae53175a37cefaf3ac601c5772c877f22453b
dpetrohilos <dylanpetrohilos@gmail.com>
2022-08-08 15:29:23 -0400

    Update scripts.js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,16 +7,15 @@
 
 
 function menuOnClick() {
-	document.getElementById("search-icon").classList.toggle("search");
-	document.getElementById("nav").classList.toggle("no-search");
-	document.getElementById("menu-bg").classList.toggle("search-active");
-  }
-  
-  document.addEventListener('keydown', function(event){
-	  if(event.key === "Escape"){
-	document.getElementById("menu-bar").classList.remove("change");
-	document.getElementById("nav").classList.remove("change");
-	document.getElementById("menu-bg").classList.remove("change-bg");
-	  }
-  });
-  
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+document.addEventListener('keydown', function(event){
+	if(event.key === "Escape"){
+  document.getElementById("menu-bar").classList.remove("change");
+  document.getElementById("nav").classList.remove("change");
+  document.getElementById("menu-bg").classList.remove("change-bg");
+	}
+});
