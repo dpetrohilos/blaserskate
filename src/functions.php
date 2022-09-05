fca3aa35bb79868594bc9476337bf7c067ea7bbb
dpetrohilos <dylanpetrohilos@gmail.com>
2022-09-05 14:27:15 -0400

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -83,10 +83,7 @@ function html5blank_nav() {
         'items_wrap'      => '',
         'depth'           => 0,
         'walker'          => '',
-        'add_li_class'  => 'page_item',
-        'add_ul_class'  => 'main-navigation',
-        'add_ul_id'  => 'primary-menu'
-
+        'add_li_class'  => 'page_item'
 
         )
     );
