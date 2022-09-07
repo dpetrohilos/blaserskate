47f049415dd76655fc08697f5c00a45ef505226f
dpetrohilos <dylanpetrohilos@gmail.com>
2022-09-07 04:37:15 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
 <section class="grid-item-1 col-1">
 <header class="content">
 <div class="over-text kent sub-title">2022</div>
-<h1 class="section-title">Handcrafted skateboards</h1>
+<h1 class="section-title">Handcrafted decks</h1>
 <p>Louisville, KY is known for two things. Very strong baseball bats, and lovingly handcrafted skateboards. Come shred with us!</p>
 <a class="button primary" href="/store">Explore</a>
 
