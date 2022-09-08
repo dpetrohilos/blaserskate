542c429c184a8212fffe09cc455a18f2b48eef43
dpetrohilos <dylanpetrohilos@gmail.com>
2022-09-08 02:41:32 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -48,12 +48,9 @@ Louisville, KY </cite>
 
 <div class="grid grid-row-3">
 <header class="grid-item-1 title">
-<h2 class="section-title">Shred The Gnar </h2>
+<h2 class="section-title">Shred The Gnar</h2>
 </header>
-<div class="grid-item-2 col-1"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/img_2.jpg"/>
-</div>
-<div class="grid-item-3 col-2"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/img_3.jpg"/></div>
-</div>
+<div class="grid-item-2 col-1"><img alt="hand crafted, made with love, picture of the skateboard. Illustration of the dragon and logo" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/handcrafted.jpg"/>
 </div>
 <div class="grid grid-row-4 col-wrapper">
 <div class="grid-item-1 col-1"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/img_4.jpg"/></div>
