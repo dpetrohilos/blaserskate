8ee95451159a6389d37b7634b3a07cd20b247eed
dpetrohilos <dylanpetrohilos@gmail.com>
2022-09-09 16:49:58 -0400

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -172,8 +172,11 @@ function html5blank_styles() {
 // Register HTML5 Blank Navigation
 function register_html5_menu() {
     register_nav_menus( array( // Using array to specify more menus if needed
-        'header-menu'  => esc_html( 'Header Menu', 'html5blank' ), // Main Navigation
-        'extra-menu'   => esc_html( 'Extra Menu', 'html5blank' ) // Extra Navigation if needed (duplicate as many as you need!)
+        'main-nav'  => esc_html( 'Main Menu', 'html5blank' ), // Main Navigation
+        'top-menu'   => esc_html( 'Top Menu', 'html5blank' ), // Extra Navigation if needed (duplicate as many as you need!)
+        'footer-nav-1'   => esc_html( 'Footer Menu', 'html5blank' ), // Extra Navigation if needed (duplicate as many as you need!)
+        'footer-nav-2'   => esc_html( 'Footer Social Menu', 'html5blank' ), // Extra Navigation if needed (duplicate as many as you need!)
+
     ) );
 }
 
