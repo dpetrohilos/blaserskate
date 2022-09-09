b27fe702f4a5067b453af766a5a38ea6b93d5134
dpetrohilos <dylanpetrohilos@gmail.com>
2022-09-09 13:50:34 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -57,13 +57,14 @@ Louisville, KY </cite>
 
 
 <div class="grid grid-row-4 col-wrapper">
-<div class="grid-item-1 col-1 title"><div classs="section-title"> We ride because we can
-</div>
+<div class="grid-item-1 col-1 title">
+	<div classs="section-title"><p>We ride because we can</p></div>
 <div class="grid-item-2 col-2"><blockquote class="pq">
 A lot of people buy a surfboard because their favorite shapers made it, not because their favorite company made it.</blockquote>
 <cite>–Korey Blaser, 
 Founder, Louisville, KY </cite>
 <img class="circle" height="84" width="84" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/people/korey.jpg"/>
+</div>
 </div>
 </div>
 <div class="grid grid-row-3 wrapper col-wrapper">
