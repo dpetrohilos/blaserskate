611b0c5007c8534bfa523f634c40180656643732
dpetrohilos <dylanpetrohilos@gmail.com>
2022-09-10 16:23:54 -0400

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -62,26 +62,30 @@ if ( function_exists( 'add_theme_support' ) ) {
 /*------------------------------------*\
     Functions
 \*------------------------------------*/
-
-// HTML5 Blank navigation
-$defaults = array(
-    'theme_location'  => ,
-    'menu'            => ,
-    'container'       => 'div',
-    'container_class' => 'menu-{menu slug}-container',
-    'container_id'    => ,
-    'menu_class'      => 'menu',
-    'menu_id'         => ,
-    'echo'            => true,
-    'fallback_cb'     => 'wp_page_menu',
-    'before'          => ,
-    'after'           => ,
-    'link_before'     => ,
-    'link_after'      => ,
-    'items_wrap'      => '<ul id=\"%1$s\" class=\"%2$s\">%3$s</ul>',
-    'depth'           => 0,
-    'walker'          =>
-);
+function wp_nav_menu( $args = array() ) {
+    static $menu_id_slugs = array();
+ 
+    $defaults = array(
+        'menu'            => '',
+        'container'       => 'div',
+        'container_class' => '',
+        'container_id'    => '',
+        'menu_class'      => 'menu',
+        'menu_id'         => '',
+        'echo'            => true,
+        'fallback_cb'     => 'wp_page_menu',
+        'before'          => '',
+        'after'           => '',
+        'link_before'     => '',
+        'link_after'      => '',
+        'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+        'item_spacing'    => 'preserve',
+        'depth'           => 0,
+        'walker'          => '',
+        'theme_location'  => '',
+    );
+ 
+    $args = wp_parse_args( $args, $defaults );
 
 
 
