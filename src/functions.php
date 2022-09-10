33394ac579bb5baf61bda7ab9e402feeed27d6d6
dpetrohilos <dylanpetrohilos@gmail.com>
2022-09-10 15:33:52 -0400

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -318,7 +318,7 @@ add_action( 'wp_enqueue_scripts', 'html5blank_header_scripts' ); // Add Custom S
 add_action( 'wp_print_scripts', 'html5blank_conditional_scripts' ); // Add Conditional Page Scripts
 add_action( 'get_header', 'enable_threaded_comments' ); // Enable Threaded Comments
 add_action( 'wp_enqueue_scripts', 'html5blank_styles' ); // Add Theme Stylesheet
-add_action( 'init', 'register_wp_nav_menu' ); // Add HTML5 Blank Menu
+add_action( 'init', 'register_html5_menu' ); // Add HTML5 Blank Menu
 // add_action( 'init', 'create_post_type_html5' ); // Add our HTML5 Blank Custom Post Type
 add_action( 'widgets_init', 'my_remove_recent_comments_style' ); // Remove inline Recent Comment Styles from wp_head()
 add_action( 'init', 'html5wp_pagination' ); // Add our HTML5 Pagination
