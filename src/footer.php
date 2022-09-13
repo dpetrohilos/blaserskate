e3276594c72ff99b6b889a6fd49bd3ea78486204
dpetrohilos <dylanpetrohilos@gmail.com>
2022-09-12 21:09:14 -0400

    Update footer.php

--- src/footer.php
+++ src/footer.php
@@ -1,6 +1,7 @@
 			<!-- footer -->
 			<footer class="footer grid" role="contentinfo">
-        <div class="footer-internal-wrapper>
+        <div class="footer-internal-wrapper">
+
 <div class="grid-item-1">
 <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/footer_img.png" alt="Handmade designs" class="footer design element">
 
