037874ace5b87bc0b7dff759823f00092b0be8d3
dpetrohilos <dylanpetrohilos@gmail.com>
2022-09-13 02:39:18 -0400

    grid grid grid

--- src/index.php
+++ src/index.php
@@ -72,7 +72,7 @@ Founder, Louisville, KY </cite>
 <div class="grid grid-row-4 center-rail">
 
 
-<div class="grid-item-1 col-1 grid first-row">
+<div class="grid-item-1 col-1 grid-row first-row">
 <blockquote class="pq">
 A lot of people buy a surfboard because their favorite shapers made it, not because their favorite company made it.</blockquote>
 <div class="h-align inline-flex v-align second-row"><div class="cite-wrapper text-center inline-flex"><cite class="v-align flex">–Korey Blaser, 
