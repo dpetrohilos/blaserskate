faba000dd12b6718b1f6de1b649a57fb0eb4d7d5
dpetrohilos <dylanpetrohilos@gmail.com>
2022-09-20 03:55:19 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -17,9 +17,9 @@
 
 
 <picture>
-    <source media="(min-width: 1000px)" srcset="red.jpg">
-    <source media="(min-width: 470px)" srcset="blue.jpg">
-	<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/grid/hero_BG_mobile.jpg"/></picture>
+    <source media="(min-width: 1000px)" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/grid/hero_BG_desktop.jpg">
+    <source media="(min-width: 470px)" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/grid/Hero_BG_tablet.jpg">
+	<img alt="person skateboarding" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/grid/hero_BG_mobile.jpg"/></picture>
 
 
 
