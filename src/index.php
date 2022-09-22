0aa8ed061ef61296da195993fa249c6c4b1ddaa1
dpetrohilos <dylanpetrohilos@gmail.com>
2022-09-22 14:51:38 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -17,9 +17,9 @@
 
 
 <picture>
-    <source media="(min-width: 1000px)" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/hero_BG_desktop.jpg">
-    <source media="(min-width: 470px)" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Hero_BG_tablet.jpg">
-	<img alt="person skateboarding" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/hero_BG_mobile.jpg"/></picture>
+    <source height="1400" width="2400" media="(min-width: 1000px)" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/hero_BG_desktop.jpg">
+    <source height="1400" width="1200" media="(min-width: 470px)" srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/img/Hero_BG_tablet.jpg">
+	<img height="600" width="900"  alt="person skateboarding" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/hero_BG_mobile.jpg"/></picture>
 
 
 
