1c718c79cb2c0c428ac6fc8230b35555f2682546
dpetrohilos <dylanpetrohilos@gmail.com>
2022-09-23 02:28:53 -0400

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -381,34 +381,22 @@ function html5_shortcode_demo_2( $atts, $content = null ) {
 
 // WOO 
 
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+// Add new constant that returns true if WooCommerce is active
+define( 'WPEX_WOOCOMMERCE_ACTIVE', class_exists( 'WooCommerce' ) );
 
-add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
-add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
-
-function my_theme_wrapper_start() {
-    echo '	<main class="grid" id="main" role="main" aria-label="Content">';
-}
-
-function my_theme_wrapper_end() {
-    echo '</main>';
+// Checking if WooCommerce is active
+if ( WPEX_WOOCOMMERCE_ACTIVE ) {
+    // Do something...
+    // Such as including a new file with all your Woo edits.
 }
 
 
-function mytheme_add_woocommerce_support() {
-    add_theme_support( 'woocommerce' );
-}
+add_action( 'after_setup_theme', function() {
+	add_theme_support( 'woocommerce' );
+} );
 
-add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
-remove_theme_support( 'wc-product-gallery-zoom' );
-remove_theme_support( 'wc-product-gallery-lightbox' );
-remove_theme_support( 'wc-product-gallery-slider' );
-add_filter( 'woocommerce_enqueue_styles', '__return_false' );
-
-// Register Sidebars
-function custom_sidebars() {
-
-}
-add_action( 'widgets_init', 'custom_sidebars' );
+add_theme_support( 'wc-product-gallery-slider' );
+add_theme_support( 'wc-product-gallery-zoom' );
+add_theme_support( 'wc-product-gallery-lightbox' );
+add_filter( 'woocommerce_show_page_title', '__return_false' );
