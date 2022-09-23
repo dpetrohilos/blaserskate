9df6da4e5a47570676f39c7472a33db21367d655
dpetrohilos <dylanpetrohilos@gmail.com>
2022-09-23 02:40:30 -0400

    pressure drop

--- src/functions.php
+++ src/functions.php
@@ -380,23 +380,40 @@ function html5_shortcode_demo_2( $atts, $content = null ) {
 
 
 // WOO 
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
 
-// Add new constant that returns true if WooCommerce is active
-define( 'WPEX_WOOCOMMERCE_ACTIVE', class_exists( 'WooCommerce' ) );
+function my_theme_wrapper_start() {
+    echo '<section id="woo">';
+}
 
-// Checking if WooCommerce is active
-if ( WPEX_WOOCOMMERCE_ACTIVE ) {
-    // Do something...
-    // Such as including a new file with all your Woo edits.
+function my_theme_wrapper_end() {
+    echo '</section>';
 }
 
 
-add_action( 'after_setup_theme', function() {
-	add_theme_support( 'woocommerce' );
-} );
+function mytheme_add_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
 
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+function mytheme_add_woocommerce_support() {
+    add_theme_support( 'woocommerce', array(
+        'thumbnail_image_width' => 150,
+        'single_image_width'    => 300,
 
-add_theme_support( 'wc-product-gallery-slider' );
-add_theme_support( 'wc-product-gallery-zoom' );
-add_theme_support( 'wc-product-gallery-lightbox' );
-add_filter( 'woocommerce_show_page_title', '__return_false' );
+        'product_grid'          => array(
+            'default_rows'    => 3,
+            'min_rows'        => 2,
+            'max_rows'        => 8,
+            'default_columns' => 4,
+            'min_columns'     => 2,
+            'max_columns'     => 5,
+        ),
+    ) );
+}
+
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+add_filter( 'woocommerce_enqueue_styles', '__return_false' );
--- src/woocommerce.php
+++ src/woocommerce.php
@@ -1,4 +1,54 @@
-<?php get_header(); ?>
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' : '; } ?><?php bloginfo( 'name' ); ?></title>
+
+		<link href="//www.google-analytics.com" rel="dns-prefetch">
+		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/favicon.ico" rel="shortcut icon">
+		<link href="<?php echo esc_url( get_template_directory_uri() ); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'rss2_url' ); ?>" />
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="<?php bloginfo( 'description' ); ?>">
+
+		<?php wp_head(); ?>
+		<script>
+		// conditionizr.com
+		// configure environment tests
+		conditionizr.config({
+			assets: '<?php echo esc_url( get_template_directory_uri() ); ?>',
+			tests: {}
+		});
+		</script>
+
+	</head>
+	<body <?php body_class(); ?>>
+
+		<!-- wrapper -->
+		<div class="wrapper">
+
+			<!-- header -->
+			<header class="header clear" role="banner">
+
+					<!-- logo -->
+					<div class="logo">
+						<a href="<?php echo esc_url( home_url() ); ?>">
+							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
+							<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/logo.svg" alt="Logo" class="logo-img">
+						</a>
+					</div>
+					<!-- /logo -->
+
+					<!-- nav -->
+					<nav class="nav" role="navigation">
+						<?php html5blank_nav(); ?>
+					</nav>
+					<!-- /nav -->
+
+			</header>
+			<!-- /header -->
 
 	<main id="woo" role="main" aria-label="Content" class="grid">
 		<!-- section -->
@@ -6,33 +56,18 @@
 
 			<h1><?php the_title(); ?></h1>
 
-		<?php if ( have_posts()) : while ( have_posts() ) : the_post(); ?>
+			<?php if ( have_posts() ) : ?>
 
 			<!-- article -->
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 <?php woocommerce_content(); ?>
 
-				<?php comments_template( '', true ); // Remove if you don't want comments. ?>
 
-				<br class="clear">
 
-				<?php edit_post_link(); ?>
 
 			</article>
-			<!-- /article -->
-
-		<?php endwhile; ?>
-
-		<?php else : ?>
-
-			<!-- article -->
-			<article>
-
-				<h2><?php esc_html_e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-
-			</article>
-			<!-- /article -->
+		
 
 		<?php endif; ?>
 
@@ -40,6 +75,38 @@
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
 
-<?php get_footer(); ?>
+	<!-- footer -->
+	<footer class="footer" role="contentinfo">
+
+<!-- copyright -->
+<?php
+	printf( '<p class="copyright">' . __( '&copy; %1$s Copyright %2$s. Powered by <a href="%3$s" title="WordPress">WordPress</a> &amp; <a href="%4$s" title="HTML5 Blank">HTML5 Blank</a>.', 'html5blank' ) . '</p>',
+		date( 'Y'),
+		esc_html( get_bloginfo( 'name' ) ),
+		'//wordpress.org',
+		'//html5blank.com'
+	);
+?>
+<!-- /copyright -->
+
+</footer>
+<!-- /footer -->
+
+</div>
+<!-- /wrapper -->
+
+<?php wp_footer(); ?>
+
+<!-- analytics -->
+<script>
+(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
+(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
+l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
+ga('send', 'pageview');
+</script>
+
+</body>
+</html>
