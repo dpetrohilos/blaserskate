8dd0ee627d7e5ce89184b5a990b203019d149966
dpetrohilos <dylanpetrohilos@gmail.com>
2022-09-23 02:44:43 -0400

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -401,20 +401,4 @@ function mytheme_add_woocommerce_support() {
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
 
-function mytheme_add_woocommerce_support() {
-    add_theme_support( 'woocommerce', array(
-        'thumbnail_image_width' => 150,
-        'single_image_width'    => 300,
-
-        'product_grid'          => array(
-            'default_rows'    => 3,
-            'min_rows'        => 2,
-            'max_rows'        => 8,
-            'default_columns' => 4,
-            'min_columns'     => 2,
-            'max_columns'     => 5,
-        ),
-    ) );
-}
-
-add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
+add_filter( 'woocommerce_enqueue_styles', '__return_false' );
