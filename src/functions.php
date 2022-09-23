51133926ed04e41e9c50451c9ea0313940cc8219
dpetrohilos <dylanpetrohilos@gmail.com>
2022-09-23 02:41:02 -0400

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -416,4 +416,3 @@ function mytheme_add_woocommerce_support() {
 }
 
 add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
-add_filter( 'woocommerce_enqueue_styles', '__return_false' );
